60461491ed732c40a34ff414d1466b67ca183e03
Tahmidur_Rabb <tr1476@nyu.edu>
2023-08-12 19:23:52 -0400

    second commit

---
+++ answer.io/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>
+          Welcome to <span style= {{color: "green"}} > Answer.io</span>
+        </h1>
+    </div>
+  );
+}
+
+export default App;
